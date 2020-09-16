23cc7756d733a2e2687b51e27dc9d72e954bf58a
leesm4909 <70628667+leesm4909@users.noreply.github.com>
2020-09-16 17:33:23 +0900

    Add files via upload

---
+++ aa2-10-rpt03/app.js
@@ -0,0 +1,29 @@
+//app.js
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.get("/", function (req, res){
+    res.send('<a href="/hello">Hello Page</a>');
+});
+
+app.get("/hello", function (req, res){
+    res.send("Hello aa10");
+});
+
+app.get("/hello", function (req, res){
+    res.send("Hello Comsi!");
+});
+
+app.get("/aa10", function (req, res){
+    res.send("Hello aa10, Comsi My first express server!!!");
+});
+
+
+
+
+
+
+var server = app.listen(port, function() {
+    console.log("Listening on port %d", server.address().port);
+});
